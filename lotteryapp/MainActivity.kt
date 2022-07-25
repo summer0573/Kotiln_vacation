d39883439e7416d5d332f0c4a40131780c083047
Jung Hyun Jin <83990983+summer0573@users.noreply.github.com>
2022-07-25 11:25:16 +0900

    주석 설명 추가

--- lotteryapp/MainActivity.kt
+++ lotteryapp/MainActivity.kt
@@ -33,6 +33,19 @@ class MainActivity : AppCompatActivity() {
             nums.text = currentNums // 생성된것 currenNums에 저장
         }
 
+        /*
+        * 처음에 프리퍼런스에서 "lottonums" 키를 통해서 문자열 값을 가져오는데 맨 처음에는 저장된게 없으므로 두 번째
+        * 인자값인 빈 문자열을 가져옴
+        * val lottoNums = pref.getString("lottonums", "")
+        * 그리고 이후 if, else 표현식을 통해서 빈 문자열인 경우에는 빈 리스트를 하나 생성하도록 하고, 그 리스트에
+        * 현재 로또 번호 저장
+        * 이후
+        * val lottoNums = pref.getString("lottonums", "")
+        * 가 실행될 때에는 저장된 번호가 있으므로 "1-2-3-4-5-6" 와 같은 값 리턴
+        * else 쪽으로 빠져서 ["1-2-3-4-5-6"] 내용을 담은 리스트 반환
+        * 매번 저장할 때마다 joinToString을 통해서 문자열로 바꿔서 저장하므로 실제 저장되는 내용은
+        * "1-2-3-4-5-6,6-5-4-3-2-1" <=이런 문자열이 저장이 됨
+        */
         //저장
         var save_btn = findViewById<Button>(R.id.save_btn)
         save_btn.setOnClickListener {
