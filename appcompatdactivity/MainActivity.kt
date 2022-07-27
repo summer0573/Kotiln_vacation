fd40fe7b733d88eaf99101a0bfde181f772cf018
Jung Hyun Jin <83990983+summer0573@users.noreply.github.com>
2022-07-27 09:50:13 +0900

    Add files via upload

--- appcompatdactivity/MainActivity.kt
+++ appcompatdactivity/MainActivity.kt
@@ -7,5 +7,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //상단 제목 표시줄 숨기기
+        supportActionBar?.hide()
+
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.add(R.id.fragment_container,
+            WeatherPageFragment.newInstance("화창", 10.0))
+        transaction.commit()
+
+
     }
 }
--- appcompatdactivity/WeatherPageFragment.kt
+++ appcompatdactivity/WeatherPageFragment.kt
@@ -1,12 +1,17 @@
 package com.example.appcompatdactivity
 
 import android.os.Bundle
+import android.text.TextPaint
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 
 class WeatherPageFragment : Fragment() {
+
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -15,6 +20,28 @@ class WeatherPageFragment : Fragment() {
         val view = inflater
             .inflate(R.layout.weather_page_fragment,
                 container, false)
+
+        val statusText = view.findViewById<TextView>(R.id.weather_status_text)
+        val temperatureText = view.findViewById<TextView>(R.id.weather_temp_text)
+        val weatherImage = view.findViewById<ImageView>(R.id.weather_icon)
+
+        statusText.text = arguments?.getString("status")
+        temperatureText.text = arguments?.getDouble("temperature").toString()
+        weatherImage.setImageResource(arguments?.getInt("res_id")!!)
+
         return view
+    }
+    companion object{
+        fun  newInstance(status : String, temperature : Double) : WeatherPageFragment {
+            val fragment = WeatherPageFragment()
+
+            val args = Bundle()
+            args.putString("status", status)
+            args.putDouble("temperature", temperature)
+            args.putInt("res_id", R.drawable.sun)
+            fragment.arguments = args
+
+            return fragment
+        }
     }
 }
