34105633dd13273845d410fa4245254a9b48f47b
Jung Hyun Jin <83990983+summer0573@users.noreply.github.com>
2022-07-28 11:18:36 +0900

    pager 추가

--- appcompatdactivity/MainActivity.kt
+++ appcompatdactivity/MainActivity.kt
@@ -2,9 +2,16 @@ package com.example.appcompatdactivity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager.widget.ViewPager
+import com.example.weatherdustchecker.MyDeserialize
 import com.example.weatherdustchecker.WeatherPageFragment
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mPager : ViewPager
+    private var lat : Double = 37.579876
+    private var lon : Double = 126.976998
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -12,10 +19,14 @@ class MainActivity : AppCompatActivity() {
         //상단 제목 표시줄 숨기기
         supportActionBar?.hide()
 
-        val transaction = supportFragmentManager.beginTransaction()
-        transaction.add(R.id.fragment_container,
-            WeatherPageFragment.newInstance(37.58, 126.98))
-        transaction.commit()
+        mPager = findViewById(R.id.pager)
+        val pagerApater = MypagerApater(supportFragmentManager)
+        mPager.adapter = pagerApater
+
+//        val transaction = supportFragmentManager.beginTransaction()
+//        transaction.add(R.id.fragment_container,
+//            DustTestFragment.newInstance(37.58,126.98))
+//        transaction.commit()
 
 
     }
