3bba394ff8a0e4e1136281eb585bc1739202fef5
Jung Hyun Jin <83990983+summer0573@users.noreply.github.com>
2022-08-01 09:30:54 +0900

    Call  추가

--- githubusersearch/GithubAPIService.kt
+++ githubusersearch/GithubAPIService.kt
@@ -10,14 +10,25 @@ import retrofit2.http.Query
 import java.lang.reflect.Type
 
 interface GithubAPIService {
-    @GET("/users/{userId}") //https://api.github.com
+    @GET("/users/{userId}")
     fun getUser(
         @Path("userId") id: String
-    )
+    ) : Call<GithubUser>
 }
 
 data class GithubUser(val id: Int, val login: String)
 
 class GithubUserDeserializer : JsonDeserializer<GithubUser> {
+    override fun deserialize(
+        json: JsonElement?,
+        typeOfT: Type?,
+        context: JsonDeserializationContext?
+    ): GithubUser {
+        val root = json?.getAsJsonObject()
+        val id = root?.getAsJsonPrimitive("id")?.asInt
+        val login = root?.getAsJsonPrimitive("login")?.asString
+
+        return GithubUser(id!!, login!!)
+    }
 
 }
