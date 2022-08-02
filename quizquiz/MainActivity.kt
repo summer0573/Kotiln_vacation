1382a95c5f0d717c1b90c9a1b2c8b214c0fbae68
Jung Hyun Jin <83990983+summer0573@users.noreply.github.com>
2022-08-02 10:44:51 +0900

    Add files via upload

--- quizquiz/MainActivity.kt
+++ quizquiz/MainActivity.kt
@@ -2,10 +2,40 @@ package com.example.quizquiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var drawerToggle : ActionBarDrawerToggle
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val drawerLayout = findViewById<DrawerLayout>(R.id.drawer_layout)
+        val navView = findViewById<NavigationView>(R.id.drawer_nav_view)
+
+        navView.setNavigationItemSelectedListener {
+            when(it.itemId) {
+                R.id.quiz_solve -> Log.d("mytag", "퀴즈 풀기")
+                R.id.quiz_manage -> Log.d("mytag", "퀴즈 관리")
+            }
+
+            drawerLayout.closeDrawers()
+
+            true
+        }
+
+        drawerToggle = object : ActionBarDrawerToggle(
+            this,
+            drawerLayout,
+            R.string.drawer_open,
+            R.string.drawer_close
+        ) {}
+        drawerToggle.isDrawerIndicatorEnabled = true
+        drawerLayout.addDrawerListener(drawerToggle)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 }
