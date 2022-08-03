06b67af107116f7b48ddaa5fa861363e6880006e
Jung Hyun Jin <83990983+summer0573@users.noreply.github.com>
2022-08-03 09:49:18 +0900

    시작 버튼 만들기

--- quizquiz/QuizFragment.kt
+++ quizquiz/QuizFragment.kt
@@ -1,13 +1,34 @@
 package com.example.quizquiz
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
-class QuizFragment : Fragment() {
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?) : View? {
-        return inflater.inflate(R.layout.quiz_list_fragment, container, false)
+class QuizFragment : Fragment(),
+        QuizStartFragment.QuizStartListener
+{
+    override fun onQuizStart() {
+        Log.d("mytag", "시작하기")
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(
+            R.layout.quiz_fragment,
+            container,
+            false
+        )
+
+        childFragmentManager
+            .beginTransaction().add(R.id.fragment_container, QuizStartFragment())
+            .commit()
+
+        return view
     }
 }
