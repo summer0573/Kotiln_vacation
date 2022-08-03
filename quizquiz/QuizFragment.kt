e7ddd4b1732d7214444746e63ba7c8147679566e
Jung Hyun Jin <83990983+summer0573@users.noreply.github.com>
2022-08-03 13:31:13 +0900

    퀴즈 결과 나타내기

--- quizquiz/QuizFragment.kt
+++ quizquiz/QuizFragment.kt
@@ -6,6 +6,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.quizquiz.database.Quiz
 import com.example.quizquiz.database.QuizDatabase
@@ -27,11 +28,19 @@ class QuizFragment : Fragment(),
         if(isCorrect) correctCount++
         currentQuizIdx++
 
-        childFragmentManager
-            .beginTransaction()
-            .replace(R.id.fragment_container,
-                QuizSolveFragment.newInstance(quizList[currentQuizIdx]))
-            .commit()
+        if(currentQuizIdx == quizList.size) {
+            childFragmentManager
+                .beginTransaction()
+                .replace(R.id.fragment_container,
+                    QuizResultFragment.newInstance(correctCount, quizList.size))
+                .commit()
+        } else {
+            childFragmentManager
+                .beginTransaction()
+                .replace(R.id.fragment_container,
+                    QuizSolveFragment.newInstance(quizList[currentQuizIdx]))
+                .commit()
+        }
     }
 
     override fun onQuizStart() {
